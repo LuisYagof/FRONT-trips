db7a9d1cdd0607fc925d658f901d605949395b4f
LuisYagof <luisyagofdez@gmail.com>
2021-05-04 16:52:53 +0200

    FIX: improve fetch hook --> error handling

--- src/hooks/Fetch.js
+++ src/hooks/Fetch.js
@@ -1,7 +1,13 @@
 async function fetchData(endpoint, options) {
-  const petition = await fetch(`http://localhost:8080/${endpoint}, ${options}`)
-  const jonson = await petition.json()
-  return jonson
+  try {
+    const petition = await fetch(`http://localhost:8080/${endpoint}, ${options}`)
+    const jonson = await petition.json()
+    return jonson
+  } catch {
+    return {
+      error: "No se ha podido realizar la petición"
+    }
+  }
 }
 
 export default fetchData;
