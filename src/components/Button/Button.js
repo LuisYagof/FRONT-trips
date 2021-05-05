fa7608d3d297d27d395319f8088b05673bb5f3a3
fAlfaro21 <falfarogr@hotmail.com>
2021-05-05 11:35:24 +0200

    Cambios en App.js, Button.js, Login.js e Index

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,10 +3,19 @@ import { Link } from "react-router-dom";
 
 const Button = (props) => {
 
+    useEffect(() => {
+        const data = async () => {
+          const newData = await fetch(`http://localhost:8080/${props.functionFetch}`)
+          .then(response => response.json())
+          .then(res => setData([...data, ...res.data]));
+        }
+        console.log("Retrieving data...")
+        data();
+    }, [])
+
     return (
         <button>{`${props.text}`}</button>
         );
-
 }
 
 export default Button;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 import './index.css';
 import App from './App';
-//import Home from './components/Onboarding';
-import Login from './components/Login/Login';
-import SignUpStudent from './components/SignUpStudent';
-import SignUpTeacher from './components/SignUpTeacher';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Switch>
-        {/* <Route exact path="/" component={Onboarding} /> */}
-        <Route path="/logUser" component={Login} />
-        <Route path="/newStudent" component={SignUpStudent} /> 
-        <Route path="/newTeacher" component={SignUpTeacher} /> 
-      </Switch>
-    </BrowserRouter>
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
