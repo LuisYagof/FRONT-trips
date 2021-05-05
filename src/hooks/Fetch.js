d413e158f9845a638431f2379137258a79320642
LuisYagof <luisyagofdez@gmail.com>
2021-05-05 10:36:48 +0200

    FEAT: slider, work in progress

--- src/hooks/Fetch.js
+++ src/hooks/Fetch.js
@@ -1,6 +1,6 @@
 async function fetchData(endpoint, options) {
   try {
-    const petition = await fetch(`http://localhost:8080/${endpoint}, ${options}`)
+    const petition = await fetch(`http://localhost:8080/${endpoint}`, options)
     const jonson = await petition.json()
     return jonson
   } catch {
