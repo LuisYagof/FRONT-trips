be826b17fc1005aa656ec084ed07dc68626e883e
LuisYagof <luisyagofdez@gmail.com>
2021-05-08 15:08:38 +0200

    FEAT: new component BtnRadio -- fixed defaultChecked

--- src/components/BtnRadio/BtnRadio.js
+++ src/components/BtnRadio/BtnRadio.js
@@ -5,13 +5,13 @@ const BtnRadio = (props) => {
   const handleClick = (e) => {
     props.handleUser(e.target.value)
   }
-  
+
   return (
     <div className="userType">
-      <input onClick={handleClick} type="radio" name="userType" id="docente" value="docentes" />
+      <input className="userType" onClick={handleClick} type="radio" name="userType" id="docente" value="docentes" />
       <label htmlFor="docente" className="label-radio docente">Docente</label>
 
-      <input onClick={handleClick} type="radio" name="userType" id="estudiante" value="estudiantes" checked />
+      <input className="userType" onClick={handleClick} type="radio" name="userType" id="estudiante" value="estudiantes" defaultChecked />
       <label htmlFor="estudiante" className="label-radio estudiante">Estudiante</label>
     </div>
   )
